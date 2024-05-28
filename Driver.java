cf030f7e7aae6046ada24504d76f3d93506305db
Grace Yan <graceyan@Graces-MacBook-Air-4.local>
2024-05-27 21:09:34 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,99 @@
+import java.io.File;
+
 public class Driver {
+	
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
+		
+		Polynomial p0 = new Polynomial();
+		printCoeffArray(p0.nonZeroCoeff);
+		printExpArray(p0.exponents);
+		
+		// testing method add --------------------------------------------------------------------------------
+		// testing two polynomials (unsorted arrays) whose coefficients do not sum to 0
+		double[] c1 = {1, 2, 1, -7};
+		int[] e1 = {1, 2, 0, 8};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = {6, 3, 5, 4, 9};
+		int[] e2 = {9, 0, 3, 1, 4};
+		Polynomial p2 = new Polynomial(c2, e2);
+		
+		Polynomial sum1 = p1.add(p2);
+		printCoeffArray(sum1.nonZeroCoeff);
+		printExpArray(sum1.exponents);
+		
+		// testing two polynomials (unsorted arrays) in which some of the coefficients do sum to 0
+		double[] c3 = {1, 2, -6, -4, 3};
+		int[] e3 = {0, 5, 1, 10, 21};
+		Polynomial p3 = new Polynomial(c3, e3);
+		double[] c4 = {-9, 7, -1, -2};
+		int[] e4 = {10, 12, 0, 5};
+		Polynomial p4 = new Polynomial(c4, e4);
+
+		Polynomial sum2 = p3.add(p4);
+		printCoeffArray(sum2.nonZeroCoeff);
+		printExpArray(sum2.exponents);
+
+		// testing two polynomials with sorted arrays
+		double[] c5 = {3};
+		int[] e5 = {2};
+		Polynomial p5 = new Polynomial(c5, e5);
+		double[] c6 = {8, 5, 4};
+		int[] e6 = {1, 3, 4};
+		Polynomial p6 = new Polynomial(c6, e6);
+
+		Polynomial sum3 = p5.add(p6);
+		printCoeffArray(sum3.nonZeroCoeff);
+		printExpArray(sum3.exponents);
+		
+		
+		// testing method evaluate --------------------------------------------------------------------------------
+		System.out.println("sum2(0.1) = " + sum2.evaluate(0.1)); // arrays sorted
+		System.out.println("p1(3.5) = " + p1.evaluate(3.5)); // arrays unsorted
+		
+		
+		// testing method hasRoot --------------------------------------------------------------------------------
+		if(sum1.hasRoot(1))
+			System.out.println("1 is a root of sum1");
 		else
-			System.out.println("1 is not a root of s");
+			System.out.println("1 is not a root of sum1");
+		double[] c = {1, -2, 1};
+		int[] e = {2, 1, 0};
+		Polynomial p = new Polynomial(c, e);
+		if(p.hasRoot(1))
+			System.out.println("1 is a root of p");
+		else
+			System.out.println("1 is not a root of p");
+		
+		// testing method multiply --------------------------------------------------------------------------------
+		Polynomial product1 = p1.multiply(p3);
+		printCoeffArray(product1.nonZeroCoeff);
+		printExpArray(product1.exponents);
+		
+		Polynomial product2 = p4.multiply(p5);
+		printCoeffArray(product2.nonZeroCoeff);
+		printExpArray(product2.exponents);
+		
+		// testing method saveToFile --------------------------------------------------------------------------------
+		p3.saveToFile("text.txt");
+		p4.saveToFile("text.txt");
+		
+		// testing polynomial constructor that accepts a file --------------------------------------------------------
+		Polynomial poly = new Polynomial(new File("text.txt"));
+		printCoeffArray(poly.nonZeroCoeff);
+		printExpArray(poly.exponents);
+	}
+	
+	// helper functions
+	public static void printCoeffArray(double[] coeff) {
+		for(int i = 0; i < coeff.length; i++) {
+			System.out.print(coeff[i] + " ");
+		}
+		System.out.println();
+	}
+	public static void printExpArray(int[] exp) {
+		for(int i = 0; i < exp.length; i++) {
+			System.out.print(exp[i] + " ");
+		}
+		System.out.println();
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,62 +1,190 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Polynomial{
 	
 	//field
-	double[] coefficients;
+	double[] nonZeroCoeff;
+	int[] exponents;
 	
 	//constructors
 	public Polynomial() {
-		coefficients = new double[0];
+		nonZeroCoeff = new double[0];
+		exponents = new int[0];
 	}
 	
-	public Polynomial(double[] input) {
+	public Polynomial(double[] coeff, int[] exp) {
 		
-		coefficients = new double[input.length];
+		// Note: cannot assume input is sorted
+			
+		nonZeroCoeff = new double[coeff.length];
+		exponents = new int[exp.length];
 		
-		for(int i = 0; i < input.length; i++) {
-			coefficients[i] = input[i];
+		for(int i = 0; i < coeff.length; i++) {
+			nonZeroCoeff[i] = coeff[i];
+			exponents[i] = exp[i];
 		}
+	}
+	
+	public Polynomial(File f) {
+		
+		Scanner input;	
+		try {
+			input = new Scanner(f);
+			String[] splitted = input.nextLine().split("(?=[+-])"); // (?=[+-]) allows for splitting at the +- signs while preserving them
+			nonZeroCoeff = new double[splitted.length];
+			exponents = new int[splitted.length];
+
+			for(int i = 0; i < splitted.length; i++) {
+				
+				String part = splitted[i];
+				
+				if(part.contains("x")) {
+					String[] splitMore = part.split("x");
+					nonZeroCoeff[i] = Double.parseDouble(splitMore[0]);
+					exponents[i] = Integer.parseInt(splitMore[1]);					
+				}
+				else { // the constant term
+					nonZeroCoeff[i] = Double.parseDouble(part);
+					exponents[i] = 0;		
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		}	
 	}
 	
 	//methods
 	public Polynomial add(Polynomial p) {
 		
-		// determining which polynomial has higher degree and set up corresponding coefficient array
+		// determining largest degree
 		double[] coeff;
+		int degree = 0;
 		
-		if(p.coefficients.length >= this.coefficients.length) {
-			coeff = new double[p.coefficients.length];
+		for(int exp : this.exponents) {
+			if(exp > degree) {
+				degree = exp;
+			}
 		}
-		else {
-			coeff = new double[this.coefficients.length];
+		for(int exp : p.exponents) {
+			if(exp > degree) {
+				degree = exp;
+			}
+		}
+
+		coeff = new double[degree + 1];
+		// add while ensuring that resulting polynomial has sorted coefficients
+		for(int deg = 0; deg <= degree; deg++) {
+			for(int expIndex = 0; expIndex < p.exponents.length; expIndex++) {
+				if(deg == p.exponents[expIndex]) {
+					coeff[deg] += p.nonZeroCoeff[expIndex];
+				}
+			}
+		}
+		for(int deg = 0; deg <= degree; deg++) {
+			for(int expIndex = 0; expIndex < this.exponents.length; expIndex++) {
+				if(deg == this.exponents[expIndex]) {
+					coeff[deg] += this.nonZeroCoeff[expIndex];
+				}
+			}
 		}
 		
-		// fill in array
-		for(int i = 0; i < p.coefficients.length; i++) {
-			coeff[i] += p.coefficients[i];
+		//determine where zero coefficients occur
+		int countZero = 0;
+		for(double currCoeff : coeff) {
+			if(currCoeff == 0.0) {
+				countZero++;
+			}
 		}
-		for(int j = 0; j < this.coefficients.length; j++) {
-			coeff[j] += this.coefficients[j];
+		// create new (possibly) smaller array without zero coefficients & create exponents array
+		double[] newCoeff = new double[coeff.length - countZero];
+		int[] exp = new int[newCoeff.length];
+		
+		int index = 0;
+		int deg = 0;
+		for(double currCoeff : coeff) {
+			if(currCoeff != 0.0) {
+				newCoeff[index] = currCoeff;
+				exp[index] = deg;
+				index++;
+			}
+			deg++;
 		}
 		
-		// return resulting polynomial
-		return new Polynomial(coeff);
+		// return resulting polynomial whose coefficients and exponent arrays are sorted
+		return new Polynomial(newCoeff, exp);
 	}
 	
-	public double evaluate(double x) {
+	public double evaluate(double x) {	
 		
-		double result = 0.0;
-		
-		for(int i = 0; i < coefficients.length; i++) {
-			result += coefficients[i] * Math.pow(x, i);
+		double result = 0.0;	
+		for(int i = 0; i < nonZeroCoeff.length; i++) {
+			result += nonZeroCoeff[i] * Math.pow(x, exponents[i]);
 		}
-		
 		return result;
 	}
 	
-	public boolean hasRoot(double root) {
+	public boolean hasRoot(double root) {	
 		
 		if(evaluate(root) == 0) return true;
-		
 		return false;
 	}
+	
+	public Polynomial multiply(Polynomial p) {
+		
+		double[] coeff = new double[this.exponents.length];
+		int[] exp = new int[this.exponents.length]; 
+		
+		// multiply first term of p with all terms of calling polynomial, store this in new polynomial
+		for(int i = 0; i < this.exponents.length; i++) {
+			coeff[i] = this.nonZeroCoeff[i] * p.nonZeroCoeff[0];
+			exp[i] = this.exponents[i] + p.exponents[0];
+		}
+		Polynomial poly = new Polynomial(coeff, exp);
+		
+		// multiply each of the rest of the terms of p with all terms of calling polynomial
+		for(int i = 1; i < p.exponents.length; i++) {
+			for(int j = 0; j < this.exponents.length; j++) {
+				coeff[j] = this.nonZeroCoeff[j] * p.nonZeroCoeff[i];
+				exp[j] = this.exponents[j] + p.exponents[i];
+			}
+			poly = poly.add(new Polynomial(coeff, exp));
+		}	
+		return poly;
+	}
+	
+	public void saveToFile(String fileName) {
+		
+		String polyStr = "";
+		
+		for(int i = 0; i < exponents.length; i++) {
+			
+			String coeff = String.valueOf(nonZeroCoeff[i]);			
+			if(nonZeroCoeff[i] > 0.0) {
+				coeff = "+" + coeff;
+			}
+			if(exponents[i] != 0) {
+				polyStr += coeff + "x" + String.valueOf(exponents[i]);
+			}
+			else {
+				polyStr += coeff;
+			}
+		}
+		
+		if(polyStr.charAt(0) == '+') {
+			polyStr = polyStr.substring(1, polyStr.length());
+		}
+		
+		try {
+			PrintStream output = new PrintStream(fileName);
+			output.append(polyStr);
+			output.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		}
+	}
+	
 }
